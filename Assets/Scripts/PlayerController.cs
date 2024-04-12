507f21233b1b2a860dd0384ccad29c1e59f3bbbd
manurec0 <72600413+manurec0@users.noreply.github.com>
2024-04-12 18:54:16 +0200

    UI text

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,23 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+
     private Rigidbody rb;
+    private int count;
+
     public float movementX;
     public float movementY;
+
     // Start is called before the first frame update
     void Start()
     {
+        count = 0;
         rb = GetComponent<Rigidbody>();
+
+        SetCountText ();
+
+        // Set the text property of the Win Text UI to an empty string, making the 'You Win' (game over message) blank
+        winTextObject.SetActive(false);
     }
+
     private void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
         rb.AddForce(movement * speed);
     }
+
     void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
@@ -26,11 +41,27 @@ public class PlayerController : MonoBehaviour
         movementY = movementVector.y;
 
     }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+
+        if (count >= 12)
+        {
+            // Set the text value of your 'winText'
+            winTextObject.SetActive(true);
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("PickUp"))
         {
+            count += 1;
             other.gameObject.SetActive(false);
+
+            // Run the 'SetCountText()' function (see below)
+            SetCountText();
         }
     }
 
